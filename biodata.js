d6b94c40e11991aa6398a4bff59a50646417221f
Muhamadakbar23 <161696492+Muhamadakbar23@users.noreply.github.com>
2024-09-24 11:24:55 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,8 @@
+const nama = document.querySelector('#nama');
+nama.textContent = 'Akbar';
+
+const TTL = document.querySelector('#TTL');
+TTL.textContent = 'Sukabumi,16 maret 2002';
+
+const Alamat = document.querySelector('#alamat');
+Alamat.textContent = 'Selabintana';
